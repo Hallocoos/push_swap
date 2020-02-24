accb454651224f1cbf39dad7492ff1b2fc19cf21
Hallocoos <45897219+Hallocoos@users.noreply.github.com>
2020-02-24 09:03:28 +0200

    a

--- sorting_algorithms1.c
+++ sorting_algorithms1.c
@@ -15,25 +15,25 @@
 void sort_two(t_stack **stack)
 {
 	if ((*stack)->weight > (*stack)->next->weight)
-		ft_sa(stack, 0);
+		ft_sa(stack, 1);
 }
 
 void sort_three(t_stack **stack)
 {
 	if ((*stack)->weight == 3 && (*stack)->next->weight == 1 && (*stack)->next->next->weight == 2)
-		ft_ra(stack, 0);
+		ft_ra(stack, 1);
 	sort_two(stack);
 	if ((*stack)->next->next->weight == 1)
-		ft_rra(stack, 0);
+		ft_rra(stack, 1);
 	else if ((*stack)->weight == 1 && (*stack)->next->weight == 3 && (*stack)->next->next->weight == 2)
 	{
-		ft_rra(stack, 0);
-		ft_sa(stack, 0);
+		ft_rra(stack, 1);
+		ft_sa(stack, 1);
 	}
 	else if ((*stack)->weight == 3 && (*stack)->next->weight == 2 && (*stack)->next->next->weight == 1)
 	{
-		ft_ra(stack, 0);
-		ft_sa(stack, 0);
+		ft_ra(stack, 1);
+		ft_sa(stack, 1);
 	}
 }
 
@@ -42,18 +42,18 @@ void sort_five(t_stack **stacka, t_stack **stackb)
 	if (stacksize(*stacka) == 5)
 	{
 		while ((*stacka)->weight != 1)
-			ft_ra(stacka, 0);
-		ft_pb(stacka, stackb, 0);
+			ft_ra(stacka, 1);
+		ft_pb(stacka, stackb, 1);
 		set_weight(*stacka);
 	}
 	while ((*stacka)->weight != 1)
-		ft_ra(stacka, 0); 
-	ft_pb(stacka, stackb, 0);
+		ft_ra(stacka, 1); 
+	ft_pb(stacka, stackb, 1);
 	set_weight(*stacka);
 
 	sort_three(stacka);
 	while (stacksize(*stackb) != 0)
-		ft_pa(stacka, stackb, 0);
+		ft_pa(stacka, stackb, 1);
 	set_weight(*stacka);
 }
 
@@ -61,20 +61,20 @@ void sort_five(t_stack **stacka, t_stack **stackb)
 void sort_three_desc(t_stack **stack)
 {
 	if ((*stack)->end == -1 && (*stack)->next->end == 1 && (*stack)->next->next->end == 0)
-		ft_ra(stack, 0);
+		ft_ra(stack, 1);
 	else {
 		sort_two(stack);
 		if ((*stack)->end == -1)
-			ft_rra(stack, 0);
+			ft_rra(stack, 1);
 		else if ((*stack)->end == 1 && (*stack)->next->end == -1 && (*stack)->next->next->end == 0)
 		{
-			ft_rra(stack, 0);
-			ft_sa(stack, 0);
+			ft_rra(stack, 1);
+			ft_sa(stack, 1);
 		}
 		else if ((*stack)->end == 0 && (*stack)->next->end == -1 && (*stack)->next->next->end == 1)
 		{
-			ft_ra(stack, 0);
-			ft_sa(stack, 0);
+			ft_ra(stack, 1);
+			ft_sa(stack, 1);
 		}
 	}
 }
@@ -103,12 +103,12 @@ t_stack	*find_smallest_move_count(t_stack *stacka)
 void sort_all(t_stack **stacka, t_stack **stackb)
 {
 	// START ==================================================
-	ft_pb(stacka, stackb, 0);
-	ft_pb(stacka, stackb, 0);
-	ft_pb(stacka, stackb, 0);
+	ft_pb(stacka, stackb, 1);
+	ft_pb(stacka, stackb, 1);
+	ft_pb(stacka, stackb, 1);
 
 	while (*stackb != find_max(*stackb))
-		ft_rb(stackb, 0);
+		ft_rb(stackb, 1);
 	find_min(*stackb);
 
 	if (is_sorted_desc(*stackb))
@@ -117,7 +117,7 @@ void sort_all(t_stack **stacka, t_stack **stackb)
 
 	// MIDDLE ==================================================
 	t_stack	*smallest_move_count;
-	// t_stack	*weight;
+	t_stack	*weight;
 	// stacka_move_count(*stacka);
 	// stackb_move_count(*stackb);
 	// find_best_stackb_pos(*stacka, *stackb);
@@ -131,46 +131,64 @@ void sort_all(t_stack **stacka, t_stack **stackb)
 		stackb_move_count(*stackb);
 		find_best_stackb_pos(*stacka, *stackb);
 		move_calc(*stacka, *stackb);
-		print_stack(stacka);
-		print_stack(stackb);
 		smallest_move_count = find_smallest_move_count(*stacka);
 		// weight = find_weight(*stackb, (*stacka)->best_pos_weight);
 		while ((*stacka)->weight != smallest_move_count->weight)
 		{
-			// smallest_move_count = find_smallest_move_count(*stacka);
-			// weight = find_weight(*stackb, (*stacka)->best_pos_weight);
-			// if (smallest_move_count->move_count > 0)
-				ft_ra(stacka, 0);
-			// else if (smallest_move_count->move_count < 0 && weight->move_count < 0)
-				// ft_rrr(stacka, stackb, 0);
-			// else if (smallest_move_count->move_count < 0)
-				// ft_rra(stacka, 0);
+			write(1, "B", 1);
+			smallest_move_count = find_smallest_move_count(*stacka);
+			weight = find_weight(*stackb, (*stacka)->best_pos_weight);
+			if (smallest_move_count->move_count < 0 && weight->move_count < 0)
+				ft_rrr(stacka, stackb, 1);
+			else if (smallest_move_count->move_count > 0 && weight->move_count > 0)
+				ft_rr(stacka, stackb, 1);
+			else if (smallest_move_count->move_count > 0)
+				ft_ra(stacka, 1);
+			else if (smallest_move_count->move_count < 0)
+				ft_rra(stacka, 1);
 		}
-		// smallest_move_count = find_smallest_move_count(*stacka);
-		// weight = find_weight(*stackb, (*stacka)->best_pos_weight);
+		weight = find_weight(*stackb, (*stacka)->best_pos_weight);
+		smallest_move_count = find_smallest_move_count(*stacka);
 		while ((*stackb)->weight != (*stacka)->best_pos_weight)
 		{
-			// smallest_move_count = find_smallest_move_count(*stacka);
-			// weight = find_weight(*stackb, (*stacka)->best_pos_weight);
-			// if (weight->move_count > 0)
-				ft_rb(stackb, 0);
-			// else if (smallest_move_count->move_count < 0 && weight->move_count < 0)
-				// ft_rrr(stacka, stackb, 0);
-			// else if (weight->move_count < 0)
-				// ft_rrb(stackb, 0);
+			weight = find_weight(*stackb, (*stacka)->best_pos_weight);
+			if (smallest_move_count->move_count < 0 && weight->move_count < 0)
+				ft_rrr(stacka, stackb, 1);
+			else if (smallest_move_count->move_count > 0 && weight->move_count > 0)
+				ft_rr(stacka, stackb, 1);
+			else if (weight->move_count > 0)
+				ft_rb(stackb, 1);
+			else if (weight->move_count < 0)
+				ft_rrb(stackb, 1);
+			ft_putstr("\nValues: ");
+			ft_putnbr((*stackb)->weight);
+			ft_putstr(" AND ");
+			ft_putnbr((*stacka)->best_pos_weight);
+			ft_putstr(";");
+			ft_putstr("\nValues: ");
+			ft_putnbr(smallest_move_count->move_count);
+			ft_putstr(" AND ");
+			ft_putnbr(weight->move_count);
+			ft_putstr(";\n");
+			stacka_move_count(*stacka);
+			stackb_move_count(*stackb);
+			find_best_stackb_pos(*stacka, *stackb);
+			move_calc(*stacka, *stackb);
+			print_stack(stacka);
+			print_stack(stackb);
 		}
-		ft_pb(stacka, stackb, 0);
+		ft_pb(stacka, stackb, 1);
 	}
 	// MIDDLE ==================================================
 
 	// END =====================================================
 	while (*stackb != find_max(*stackb))
-		ft_rb(stackb, 0);
+		ft_rb(stackb, 1);
 
 	// stacka_move_count(*stacka);
 	// stackb_move_count(*stackb);
 
 	while (*stackb)
-		ft_pa(stacka, stackb, 0);
+		ft_pa(stacka, stackb, 1);
 	// END =====================================================
 }
